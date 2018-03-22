81d9623d6d9ea59850a46a6a09bea97123702ea6
Ruby Pruneda <rubypruneda@outlook.com>
2018-03-22 10:49:29 -0600

    reddit/watson working!

--- server/index.js
+++ server/index.js
@@ -17,7 +17,7 @@ const {
     WATSON_USERNAME,
     PASSWORD
 } = process.env;
-console.log(WATSON_USERNAME, PASSWORD);
+// console.log(WATSON_USERNAME, PASSWORD);
 
 const app = express();
 app.use(bodyParser.json());
@@ -30,6 +30,10 @@ const r = new snoowrap({
     refreshToken: REFRESH_TOKEN
 });
 
+
+
+//-----------watson/reddit--------------
+
 app.post('/api/user', (req, res) => {
     console.log(req.body.user);
     r.getUser(req.body.user).getComments().then(cmt => { // any reddit user can be passed into getUser
@@ -38,70 +42,56 @@ app.post('/api/user', (req, res) => {
             userStr += cmt[i].body + ' '
         } 
         console.log(userStr);
+        //watson api below
+        if(userStr.length > 0){
+            // console.log(myResponse);
+            
+            // The text that we want to analyze the tone of.
+            var text = userStr;
+            
+            // Turn our text into valid json.
+            var input = { "text": text };
+            
+            // The format that the tone analyzer needs. 
+            var params = 
+                    {
+                    'tone_input': input,
+                    'content_type': 'application/json'
+                    };
+            
+            // Initialize the Tone Analyzer by giving it our credentials.
+            var tone_analyzer = new ToneAnalyzerV3(
+                    {
+                    username: WATSON_USERNAME,
+                    password: PASSWORD,
+                    version_date: '2017-09-21'
+                    });
+            
+            // Use our Tone Analyzer variable to analyze the tone.
+            tone_analyzer.tone(params, function(error, response) 
+                    {
+                    // There's an error.
+                    if (error)
+                            {
+                            console.log('Error:', error);
+                            }
+                    // No error, we got our tone result.
+                    else
+                            {
+                            // The tone of the text, as determined by watson.
+                            var tone = JSON.stringify(response, null, 2)
+                            
+                            // Output Watson's tone analysis to the console.
+                            console.log("The tone analysis for \'" + text + "\' is:\n");
+                            console.log(tone);
+                            }
+                    });
+            } else {
+                    console.log(myResponse);
+            }})
     })
-})
-// r.getUser('testdevbot').getComments().then(cmt => { // any reddit user can be passed into getUser
-//     var userStr = ''
-//     for(let i = 0; i < cmt.length; i++){
-//         userStr += cmt[i].body + ' '
-//     } 
-//     console.log(userStr);
-// })
-
-// -----Watson API-------
-
-var myResponse = '';
-r.getComment('dw27p8z').fetch().then(comment=>{
-        myResponse = comment.body;
-        // console.log(myResponse);
 
 
-if(myResponse.length >0){
-console.log(myResponse);
-
-// The text that we want to analyze the tone of.
-var text = myResponse
-
-// Turn our text into valid json.
-var input = { "text": text };
-
-// The format that the tone analyzer needs. 
-var params = 
-        {
-        'tone_input': input,
-        'content_type': 'application/json'
-        };
-
-// Initialize the Tone Analyzer by giving it our credentials.
-var tone_analyzer = new ToneAnalyzerV3(
-        {
-        username: WATSON_USERNAME,
-        password: PASSWORD,
-        version_date: '2017-09-21'
-        });
-
-// Use our Tone Analyzer variable to analyze the tone.
-tone_analyzer.tone(params, function(error, response) 
-        {
-        // There's an error.
-        if (error)
-                {
-                console.log('Error:', error);
-                }
-        // No error, we got our tone result.
-        else
-                {
-                // The tone of the text, as determined by watson.
-                var tone = JSON.stringify(response, null, 2)
-                
-                // Output Watson's tone analysis to the console.
-                console.log("The tone analysis for \'" + text + "\' is:\n");
-                console.log(tone);
-                }
-        });
-} else {
-        console.log(myResponse);
-}})
 
 
 
