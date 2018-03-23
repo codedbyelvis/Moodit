511b4b8cee3c5de919f8fdcd12750c3200d617c3
José Jaime Carvajal Calixto <joseja_17@hotmail.com>
2018-03-23 16:31:40 -0600

    storing reddit/watson data in the reducer

--- src/ducks/reducer.js
+++ src/ducks/reducer.js
@@ -1,25 +1,31 @@
 import axios from 'axios'
 const initialState = {
-    user: '',
-    text: '',
-    mood: ''
+    user: 'Bob',
+    redditData: [],
+    watsonData: []
 }
 
-const GET_USER = 'GET_USER';
-const GET_TEXT = 'GET_TEXT';
-const GET_MOOD = 'GET_MOOD';
+const GET_USERINFO = 'GET_USERINFO';
+const GET_USERINFO_PENDING = 'GET_USERINFO_PENDING'
+const GET_USERINFO_FULFILLED = 'GET_USERINFO_FULFILLED'
+
+// const GET_TEXT = 'GET_TEXT';
+// const GET_MOOD = 'GET_MOOD';
 
 export default function reducer(state = initialState, action) {
     switch (action.type) {
 
-        case GET_USER:
-            return Object.assign({}, state, { user: action.payload });
+        // case GET_USERINFO_PENDING:
+        //     return Object.assign({}, state, {loading: true});
 
-        case GET_TEXT:
-            return Object.assign({}, state, { text: action.payload });
+        case GET_USERINFO_FULFILLED:
+            return Object.assign({}, state, {user: action.payload.data.reddit[0].author, redditData: action.payload.data.reddit, watsonData: action.payload.data.watson } );
 
-        case GET_MOOD:
-            return Object.assign({}, state, { mood: action.payload });
+        // case GET_TEXT:
+        //     return Object.assign({}, state, { text: action.payload });
+
+        // case GET_MOOD:
+        //     return Object.assign({}, state, { mood: action.payload });
 
         default: 
         return state;
@@ -28,26 +34,30 @@ export default function reducer(state = initialState, action) {
 
 export function getUser( user ){
     console.log(user)
-     axios.post('/api/user', {user}).then(
-         resp => {console.log(resp) }
-     )
+    const info = axios.post('/api/user', {user}).then(
+         resp => {
+             console.log(resp)
+             return resp;
+            }
+        )
+        console.log(info);
     return{
-        type: GET_USER,
-        payload: user
+        type: GET_USERINFO,
+        payload: info
     }
 }
 
-export function getText( text ){
-    console.log(text);
-    return{
-        type: GET_TEXT,
-        payload: text
-    }
-}
+// export function getText( text ){
+//     console.log(text);
+//     return{
+//         type: GET_TEXT,
+//         payload: text
+//     }
+// }
 
-export function getMood( mood ){
-    return{
-        type: GET_MOOD,
-        payload: mood
-    }
-}
+// export function getMood( mood ){
+//     return{
+//         type: GET_MOOD,
+//         payload: mood
+//     }
+// }
