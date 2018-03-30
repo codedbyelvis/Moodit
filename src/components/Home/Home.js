9ffa20cbdb6ff8752511ad61d60a717858a88cff
pcdavis <pcdavis@users.noreply.github.com>
2018-03-30 15:06:21 -0600

    home page has video able to play in hero

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -12,6 +12,9 @@ import MoodditText from "../SVG/MoodditText";
 import img1 from "../../assets/img1.jpg";
 import img2 from "../../assets/img2.jpg";
 import img3 from "../../assets/img3.jpg";
+import happy_reddit from "../../assets/happy_reddit.svg";
+import './Home.css'
+
 
 class Home extends Component {
   constructor(props) {
@@ -31,34 +34,58 @@ class Home extends Component {
   render() {
     return (
       <div>
-        <img src={img1} />
 
-        <div className="myinput">
-          <form>
-            <p className="h5 text-center mb-4">Mood Analyzer</p>
-            <Input
-              label="Type Username"
-              icon="user"
-              validate
-              error="wrong"
-              success="right"
-              className="inputModal2_Title"
-              placeholder="username"
-              onChange={e => this.setState({ newUser: e.target.value })}
-            />
-
-            <div className="text-center">
-              <Link to="/profile">
-                <Button
-                  color="primary"
-                  onClick={() => this.props.getUser(this.state.newUser)}
-                >
-                  Search
-                </Button>
-              </Link>
-            </div>
-          </form>
+        <div className="home_hero">
+          {/* <video src={video1} autoPlay="true" loop > </video> */}
+          <img src={img1} />
         </div>
+
+        <div className="home_profile_login">
+        <div></div>
+
+
+          <img src={happy_reddit} />
+
+
+          <div className="home_login" >
+
+            <form>
+              <h1 >What's your moodit?</h1>
+              <h1 >Let Watson analyze you.</h1>
+
+              <Input
+                label="Type Username"
+                icon="user"
+                validate
+                error="wrong"
+                success="right"
+                className="inputModal2_Title"
+                placeholder="username"
+                onChange={e => this.setState({ newUser: e.target.value })}
+              />
+  
+              <div className="text-center">
+                <Link to="/profile">
+                  <Button
+                    color="primary"
+                    onClick={() => this.props.getUser(this.state.newUser)}
+                    >
+                    Search
+                  </Button>
+                </Link>
+              </div>
+            </form>
+          </div>
+
+
+                  <div></div>
+        </div>
+
+      <div className="home_section3">
+      <img src={img3} />
+
+      </div>
+
       </div>
     );
   }
