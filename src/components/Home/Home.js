8c668054718bfc3dbe38fe4e93cddc981d2b10a0
pcdavis <pcdavis@users.noreply.github.com>
2018-03-30 10:53:46 -0600

    login working on home page

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -1,25 +1,25 @@
 import React, { Component } from "react";
 import axios from "axios";
-// import { Link } from "react-router-dom";
-// import MyCarousel from "../Carousel/MyCarousel";
-import PLine from "../PaulsChart/PLine";
+import { Link } from "react-router-dom";
+import { connect } from 'react-redux'
+import { getUser } from '../../ducks/reducer';
+
+import { Input, Button } from "mdbreact";
 // import SvgReddit from "../SVG/SvgReddit";
 import RedditMoods from "../SVG/RedditMoods";
 import MoodditText from "../SVG/MoodditText";
 // import MyNavbar from '../Navbar/navBar'
+import img1 from "../../assets/img1.jpg";
+import img2 from "../../assets/img2.jpg";
+import img3 from "../../assets/img3.jpg";
 
 class Home extends Component {
   constructor(props) {
-    super(props);
+    super(props)
     this.state = {
-      userId: "7tzazd",
-      sampleData: {
-        id: 7,
-        post:
-          "I know a fair amount of procedural PHP and I decided I might at least learn the basics of object oriented php as well, if nothing else just to compare and see what it's like."
-      }
-    };
-  }
+        newUser: ""
+    }
+}
 
   testEndpoint() {
     console.log(this.state.fakeData);
@@ -31,15 +31,44 @@ class Home extends Component {
   render() {
     return (
       <div>
+        <img src={img1} />
 
-        <h1>Below is my svg</h1>
-        
-        
-        
-        
+        <div className="myinput">
+          <form>
+            <p className="h5 text-center mb-4">Mood Analyzer</p>
+            <Input
+              label="Type Username"
+              icon="user"
+              validate
+              error="wrong"
+              success="right"
+              className="inputModal2_Title"
+              placeholder="username"
+              onChange={e => this.setState({ newUser: e.target.value })}
+            />
+
+            <div className="text-center">
+              <Link to="/profile">
+                <Button
+                  color="primary"
+                  onClick={() => this.props.getUser(this.state.newUser)}
+                >
+                  Search
+                </Button>
+              </Link>
+            </div>
+          </form>
+        </div>
       </div>
     );
   }
 }
 
-export default Home;
+function mapStateToProps(state) {
+  const { user } = state;
+  return {
+      user
+  }
+}
+
+export default connect(mapStateToProps, { getUser })(Home);
