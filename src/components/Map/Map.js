4476f7c656f205cb6d290850827df13f97e707aa
pcdavis <pcdavis@users.noreply.github.com>
2018-04-02 09:42:15 -0600

    map axios call included

--- src/components/Map/Map.js
+++ src/components/Map/Map.js
@@ -111,3 +111,68 @@ class Map extends Component {
 }
 
 export default Map;
+
+
+// Jose Jaime Carvajal [9:40 AM]
+// import React, { Component } from "react";
+// // import { Link } from "react-router-dom";
+// import axios from 'axios'
+
+// class Map extends Component {
+//  constructor(props) {
+//    super(props)
+//    this.state = {
+//      mapInformation: [],
+//      loading: true
+//    }
+//  }
+
+//  getInfo() {
+//    axios.get('/api/comments').then(({data}) => {
+//      console.log(data)
+//      this.setState({
+//        mapInformation: data,
+//        loading: false
+//      })
+//    })
+//  }
+
+//  render() {
+//    let random = this.state.mapInformation.map((info, i) => {
+//      return(
+//       <div key={i}>
+
+//         <a href={info.link} target="_blank">{info.postTitle}</a>
+       
+//       </div>
+       
+//      )
+     
+//    })
+//    return (
+//      <div>
+//        {!this.state.loading === true ?
+//        <div>
+// <h1>Map Page</h1>
+//        <button onClick={() => this.getInfo()}>Click Me</button>
+//        {random}
+//        </div>
+       
+//        :
+//        <div>
+       
+//          <div>Waiting for your data....</div>
+//          <img
+//            src="loading.svg"
+//          />
+//        </div>
+//        }
+       
+       
+
+//      </div>
+//    );
+//  }
+// }
+
+// export default Map;
